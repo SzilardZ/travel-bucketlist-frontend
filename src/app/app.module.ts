c2c6ba1f472db222632268538218cb947159f148
SzilardZ <szilard.zr@gmail.com>
2019-04-17 21:58:35 +0200

    bucket list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BucketListComponent } from './bucket-list/bucket-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BucketListComponent
   ],
   imports: [
     BrowserModule
