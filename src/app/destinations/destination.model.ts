5bfe1c169f469610b9cb3aae1b41af611af6c316
SzilardZ <szilard.zr@gmail.com>
2019-04-19 19:24:20 +0200

    destination model created

---
+++ src/app/destinations/destination.model.ts
@@ -0,0 +1,10 @@
+export class Destination {
+
+  private location: string;
+  private note: string;
+
+  constructor(location: string, note: string) {
+    this.location = location;
+    this.note = note;
+  }
+}
