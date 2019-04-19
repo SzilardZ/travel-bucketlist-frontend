f58ba68aa412275ee35efbc94747accaf80409b9
SzilardZ <szilard.zr@gmail.com>
2019-04-19 18:54:57 +0200

    naviation component created, navbar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BucketListComponent } from './bucket-list/bucket-list.component';
 import { DestinationComponent } from './destination/destination.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import {RouterModule} from '@angular/router';
+import { DestinationsComponent } from './destinations/destinations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BucketListComponent,
-    DestinationComponent
+    DestinationComponent,
+    NavigationComponent,
+    DestinationsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
