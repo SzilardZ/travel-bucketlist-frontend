4624595ba48ae5f99685c18e2b1a436c81d2a60d
SzilardZ <szilard.zr@gmail.com>
2019-04-23 13:53:06 +0200

    visited destinations component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DestinationComponent } from './destinations/destination/destination.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { DestinationsComponent } from './destinations/destinations.component';
 import { AddNewDestinationComponent } from './destinations/add-new-destination/add-new-destination.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { VisitedDestinationsComponent } from './destinations/visited-destinations/visited-destinations.component';
+
+const appRoutes: Routes = [
+  {path: 'visited-destinations', component: VisitedDestinationsComponent}
+];
 
 @NgModule({
   declarations: [
@@ -17,6 +22,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     NavigationComponent,
     DestinationsComponent,
     AddNewDestinationComponent,
+    VisitedDestinationsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/destinations/visited-destinations/visited-destinations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visited-destinations',
+  templateUrl: './visited-destinations.component.html',
+  styleUrls: ['./visited-destinations.component.css']
+})
+export class VisitedDestinationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
