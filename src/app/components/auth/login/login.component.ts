5ae1997bea3005e4c4da73242975b6bc48a469ee
SzilardZ <szilard.zr@gmail.com>
2019-05-21 11:24:27 +0200

    login component implemented

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthLoginInfo} from '../../../utils/login-info';
+import {AuthService} from '../../../services/auth/auth.service';
+import {TokenStorageService} from '../../../services/auth/token-storage/token-storage.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router,
+              private tokenStorage: TokenStorageService) {}
+
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      'username': new FormControl(null, [Validators.required]),
+      'password': new FormControl(null, [Validators.required]),
+    });
   }
 
+  onSubmit(){
+    const data = this.loginForm.value;
+    const loginForm = new AuthLoginInfo(data['username'], data['password']);
+    this.authService.attemptAuth(loginForm).subscribe(
+      data => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUsername(data.username);
+        this.tokenStorage.saveAuthorities(data.authorities);
+
+        this.authService.lodgedIn = true;
+        console.log('OK');
+        this.router.navigate(['/lodgings/rented']);
+      },
+      error => {
+        console.log(error);
+
+      }
+    );
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -4,6 +4,7 @@ import {AuthLoginInfo} from '../../utils/login-info';
 import {Observable} from 'rxjs';
 import {SignUpInfo} from '../../utils/signup-info';
 import {JwtResponse} from '../../security/jwt-response';
+import {TokenStorageService} from './token-storage/token-storage.service';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,8 +17,9 @@ export class AuthService {
 
   private loginUrl = 'http://localhost:8080/api/auth/signin';
   private signupUrl = 'http://localhost:8080/api/auth/signup';
+  private _lodgedIn = false;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private token: TokenStorageService) {
   }
 
   attemptAuth(credentials: AuthLoginInfo): Observable<JwtResponse> {
@@ -26,5 +28,18 @@ export class AuthService {
 
   signUp(info: SignUpInfo): Observable<string> {
     return this.http.post<string>(this.signupUrl, info, httpOptions);
+  }
+
+  isLogedIn(){
+    return this.token.getToken() !== null;
+  }
+
+  set lodgedIn(value: boolean) {
+    this._lodgedIn = value;
+  }
+
+  signOut() {
+    window.sessionStorage.clear();
+    this.lodgedIn = false;
   }
 }
