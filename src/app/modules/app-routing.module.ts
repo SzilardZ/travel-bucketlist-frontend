4cc9b14b5c0bbf16ab1adecbc909ec8e39b28d27
SzilardZ <szilard.zr@gmail.com>
2019-05-23 15:26:30 +0200

    add new paths

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -5,15 +5,17 @@ import {RegisterComponent} from '../components/auth/register/register.component'
 import {LoginComponent} from '../components/auth/login/login.component';
 import {AuthGuardService} from '../services/auth/auth-guard/auth-guard.service';
 import {LogoutComponent} from '../components/auth/logout/logout.component';
-import {DestinationsComponent} from '../components/destinations/destinations.component';
+import {UserEditComponent} from '../components/user/user-edit/user-edit.component';
+import {HomeComponent} from '../components/home/home.component';
 
 const appRoutes: Routes = [
-  //{path: '', redirectTo: '/destinations', pathMatch: 'full'},
-  {path: 'destinations', component: DestinationsComponent},
-  {path: 'visited-destinations', component: VisitedDestinationsComponent},
+  {path: '', redirectTo: '/destinations', pathMatch: 'full'},
+  {path: 'destinations', canActivate: [AuthGuardService], component: HomeComponent},
+  {path: 'visited-destinations', canActivate: [AuthGuardService], component: VisitedDestinationsComponent},
+  {path: 'user/edit', canActivate: [AuthGuardService], component: UserEditComponent},
   {path: 'registration', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'logout', canActivate: [AuthGuardService], component: LogoutComponent}
+  {path: 'logout', canActivate: [AuthGuardService], component: LogoutComponent},
 ];
 
 @NgModule({
